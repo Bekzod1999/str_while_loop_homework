b6ccf4d9fc9d0d36125906ece23a36194a4a2dce
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-25 10:34:27 +0500

    10

--- While10.py
+++ While10.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    j=0
+    i=0
+    while i < len(s):
+        if int(s[i]) % 2:
+            j += int(s[i])
+        i += 1
+    return j
+x=main('98421')
+print(x)
